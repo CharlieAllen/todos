8f81cc62134ed08202de17c651da5def9644bccc
Charlie Allen <miss.neophyte@gmail.com>
2016-04-08 11:26:28 +0100

    user class

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
-root to: "todos#index"
+	root to: "todos#index"
 
-resources :todos, only: [:new, :create, :index]
-resource :session, only: [:new, :create]
+	resources :todos, only: [:index, :new, :create] do
+		resource :completion, only: :create 
+	end
+	
+	resource :session, only: [:new, :create]
 end
